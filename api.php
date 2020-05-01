5f6d7e6c2048a368ea0eea2f8ca3e5d0c394a184
bpncodes <131p11v@gmail.com>
2020-05-01 18:11:53 +0530

    another php

--- api.php
+++ api.php
@@ -13,7 +13,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM housedetails";
+$sql = "SELECT * FROM tale";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
--- temp/insert_multiple.php
+++ temp/insert_multiple.php
@@ -7,6 +7,7 @@ $dbname = 'amazing_information';
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
