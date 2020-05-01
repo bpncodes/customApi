9a8607ab459276bd93f2576de3633c74d4f0704b
bpncodes <131p11v@gmail.com>
2020-05-01 13:34:07 +0530

    another php

---
+++ add.php
@@ -0,0 +1,25 @@
+<?php
+
+$username = 'bipin';
+$servername = 'localhost';
+$password = 'Bipin@123';
+$dbname = 'amazing_information';
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('Bipin ', 'Karki', 'bipin@example.com')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
