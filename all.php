a9455aa372d1be09772d1f615846a1d7ae502483
bpncodes <131p11v@gmail.com>
2020-05-01 14:33:03 +0530

    another php

--- all.php
+++ all.php
@@ -50,27 +50,3 @@ if (mysqli_multi_query($conn, $sql)) {
 
 mysqli_close($conn);
 ?>
-
-CREATE TABLE Countries (
-    Id INT NOT NULL AUTO_INCREMENT,
-    ISO VARCHAR(2) NOT NULL,
-    ISO3 VARCHAR(3) NOT NULL,
-    ISONumeric INT NOT NULL,
-    CountryName VARCHAR(64) NOT NULL,
-    Capital VARCHAR(64) NOT NULL,
-    ContinentCode VARCHAR(2) NOT NULL,
-    CurrencyCode VARCHAR(3) NOT NULL,
-    PRIMARY KEY(Id)
-)
-;
-
-INSERT INTO Countries
-    (ISO, ISO3, ISONumeric, CountryName, Capital, ContinentCode, CurrencyCode)
-VALUES
-    ('AU', 'AUS', 36, 'Australia', 'Canberra', 'OC', 'AUD'),
-    ('DE', 'DEU', 276, 'Germany', 'Berlin', 'EU', 'EUR'),
-    ('IN', 'IND', 356, 'India', 'New Delhi', 'AS', 'INR'),
-    ('LA', 'LAO', 418, 'Laos', 'Vientiane', 'AS', 'LAK'),
-    ('US', 'USA', 840, 'United States', 'Washington', 'NA', 'USD'),
-    ('ZW', 'ZWE', 716, 'Zimbabwe', 'Harare', 'AF', 'ZWL')
-;
